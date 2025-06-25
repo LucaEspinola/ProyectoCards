b2e56d67f1a58976d4df948f912a38ea78f8a91c
Agusstims <agusalvarado5@gmail.com>
2025-06-25 19:51:36 -0300

    implementacion de login

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index-page'),
-    path('login/', views.index_page, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('home/', views.home, name='home'),
     
     path('buscar/', views.search, name='buscar'),
